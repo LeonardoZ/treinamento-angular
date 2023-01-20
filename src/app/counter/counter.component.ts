3e773a0738ed8f873df8eecc4f04204f1ee13d49
Leonardo Zapparoli <leo.zapparoli@gmail.com>
2023-01-20 10:22:35 -0300

    implementa previous numbers

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -7,12 +7,15 @@ import { Component } from '@angular/core';
 })
 export class CounterComponent {
   counter = 0;
+  previousNumbers: number[] = [];
 
   increment() {
+    this.previousNumbers.push(this.counter);
     this.counter += 1;
   }
 
   decrement() {
+    this.previousNumbers.push(this.counter);
     this.counter -= 1;
   }
 }
