8090817b3a1d4c346fc18ce4ce3fd0ae0c85f9e2
Leonardo Zapparoli <leo.zapparoli@gmail.com>
2023-01-20 10:12:37 -0300

    implementa contador

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
-  styleUrls: ['./counter.component.css']
+  styleUrls: ['./counter.component.css'],
 })
 export class CounterComponent {
+  counter = 0;
 
+  increment() {
+    this.counter += 1;
+  }
+
+  decrement() {
+    this.counter -= 1;
+  }
 }
