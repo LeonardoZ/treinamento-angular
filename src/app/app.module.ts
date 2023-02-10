1545f689442b122e118a43d3bfd3bf47c9be4fc8
Leonardo Zapparoli <leo.zapparoli@gmail.com>
2023-02-10 08:07:42 -0300

    fix example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
 import { EvenOddDisplayComponent } from './even-odd-display/even-odd-display.component';
+import { PetsModule } from './pets/pets.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CounterComponent,
-    EvenOddDisplayComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CounterComponent, EvenOddDisplayComponent],
+  imports: [BrowserModule, AppRoutingModule, PetsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
